d5044495a8b241e092b6b07904528bfba9afdf25
Guangwen Zhou <zgwmike@hotmail.com>
2018-10-01 11:51:39 -0700

    Update ClusterShardingSupport.scala

--- src/main/g8/src/main/scala/$package$/application/ClusterShardingSupport.scala
+++ src/main/g8/src/main/scala/$package$/application/ClusterShardingSupport.scala
@@ -3,53 +3,58 @@ package $package$.application
 import akka.cluster.sharding.ShardRegion
 
 object ClusterShardingSupport {
-
+  type Msg = Any
   sealed trait WithEntityId {
-    def entityId: Int
+    def entityId: String
   }
 
-  trait EntityEnvelop extends WithEntityId
+  trait EntityEnvelop extends WithEntityId {
+    def body: Msg
+  }
 
 }
 
 /**
-  * Cluster sharding common functions
-  */
+ * Cluster sharding common functions
+ */
 trait ClusterShardingSupport {
 
   import ClusterShardingSupport._
 
   /**
-    * Number of shard for this type
-    *
-    * @return
-    */
+   * Number of shard for this type
+   *
+   * @return
+   */
   def numberOfShards: Int = 100
 
   /**
-    * shard type name
-    *
-    * @return
-    */
+   * shard type name
+   *
+   * @return
+   */
   def shardTypeName: String
 
   /**
-    * extract entity id from message
-    *
-    * @return
-    */
+   * extract entity id from message
+   *
+   * @return
+   */
   def extractEntityId: ShardRegion.ExtractEntityId = {
-    case msg: EntityEnvelop => (msg.entityId.toString, msg)
+    case msg: EntityEnvelop => msg.body match {
+      case Some(cmd) => (msg.entityId, cmd)
+      case _ => (msg.entityId, msg)
+    }
   }
 
   /**
-    * extract shard id from message
-    *
-    * @return
-    */
+   * extract shard id from message
+   *
+   * @return
+   */
   def extractShardId: ShardRegion.ExtractShardId = {
-    case msg: EntityEnvelop => (msg.entityId % numberOfShards).toString
+    case msg: EntityEnvelop => (msg.entityId.hashCode % numberOfShards).toString
     case ShardRegion.StartEntity(id) =>
-      (id.toLong % numberOfShards).toString
+      (id.hashCode % numberOfShards).toString
   }
 }
